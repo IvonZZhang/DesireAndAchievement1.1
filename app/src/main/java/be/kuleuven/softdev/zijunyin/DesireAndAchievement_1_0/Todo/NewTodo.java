f723b6624aa6dab3dbfb7c8223f11b86e8417488
ZZZoe <37062794+ZijunYin@users.noreply.github.com>
2018-05-28 00:26:52 +0200

    zijun version with add new habit and reward and todo but fail to add todo and reward

--- app/src/main/java/be/kuleuven/softdev/zijunyin/DesireAndAchievement_1_0/Todo/NewTodo.java
+++ app/src/main/java/be/kuleuven/softdev/zijunyin/DesireAndAchievement_1_0/Todo/NewTodo.java
@@ -12,15 +12,25 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import be.kuleuven.softdev.zijunyin.DesireAndAchievement_1_0.R;
 
 public class NewTodo extends AppCompatActivity{
-    private TextView choosenDDL;
-    private TextView choosenParent;
+    private TextView chosenDDL;
+    private TextView chosenParent;
+    private String chosenDDL_string;
     //选择日期Dialog
     private DatePickerDialog datePickerDialog;
 
@@ -37,13 +47,13 @@ public class NewTodo extends AppCompatActivity{
 
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
         String date=sdf.format(new java.util.Date());
-        choosenDDL = findViewById(R.id.choose_ddl);
-        choosenDDL.setText(date);
+        chosenDDL = findViewById(R.id.choose_ddl);
+        chosenDDL.setText(date);
 
         calendar = Calendar.getInstance();
 
-        choosenParent = findViewById(R.id.parent);
-        choosenParent.setText(R.string.none);
+        chosenParent = findViewById(R.id.parent);
+        chosenParent.setText(R.string.none);
 
     }
 
@@ -57,7 +67,51 @@ public class NewTodo extends AppCompatActivity{
     }
 
     public void finishSaveTodo(View view){
-        //这里还要写怎么上传数据
+        String todo_name;
+        int coins;
+
+
+        EditText editText1 = findViewById(R.id.new_todo_name);
+        todo_name = editText1.getText().toString();
+        EditText editText2 =(EditText)findViewById(R.id.todo_coin_number);
+        coins = Integer.parseInt(editText2.getText().toString());
+
+
+        String url = "http://api.a17-sd603.studev.groept.be/add_todo/"+
+                todo_name + "/" +
+                chosenDDL_string + "/" +
+                coins + "/" +
+                "0";
+
+        //final ArrayList<HabitDataModel> habitArray = new ArrayList<HabitDataModel>();
+        //HabitDataModel habitDataModel = new HabitDataModel();
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getBaseContext());
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //mTextView.setText("Response is: "+ response.substring(0,500));
+                        System.out.println(response);
+
+                        //parse JSON to String
+                        //String result = "";
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //mTextView.setText("That didn't work!");
+                System.out.println("failed to work");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
         finish();
     }
 
@@ -81,7 +135,8 @@ public class NewTodo extends AppCompatActivity{
                     @Override
                     public void onDateSet(DatePicker view, int year,
                                           int monthOfYear, int dayOfMonth) {
-                        choosenDDL.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+                        chosenDDL.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+                        chosenDDL_string = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
                     }
                 }
                 , calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)
